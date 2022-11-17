a91e85f392d378f990383fa31422873189ca2d91
devEMEL <dominichenry882@gmail.com>
2022-11-17 17:45:47 +0100

    Added pyteal helpers

--- project/contracts/smartContract.py
+++ project/contracts/smartContract.py
@@ -1,4 +1,5 @@
 from pyteal import *
+from pyteal_helpers import program
 from pyteal.types import TealType
 from pyteal.ast.bytes import Bytes
 from pyteal.ast.expr import Expr
@@ -16,7 +17,6 @@ def approval_program():
 	op_reveal = Bytes("reveal")
 
 
-
 	@Subroutine(TealType.none)
 	def reset(account: Expr):
 		return Seq(
@@ -26,9 +26,50 @@ def approval_program():
 			App.localPut(account, local_reveal, Bytes(""))
 		)
 
+	@Subroutine(TealType.uint64)
+	def is_empty(account: Expr):
+		return Return(
+            And(
+                App.localGet(account, local_opponent) == Bytes(""),
+                App.localGet(account, local_wager) == Int(0),
+                App.localGet(account, local_commitment) == Bytes(""),
+                App.localGet(account, local_reveal) == Bytes(""),
+            )
+        )
+
 	@Subroutine(TealType.none)
 	def create_challenge():
-		return Reject()
+		return Seq(
+            # basic sanity checks
+            program.check_self(
+                group_size=Int(2),
+                group_index=Int(0),
+            ),
+            program.check_rekey_zero(2),
+            Assert(
+                And(
+                    # second transaction is wager payment
+                    Gtxn[1].type_enum() == TxnType.Payment,
+                    Gtxn[1].receiver() == Global.current_application_address(),
+                    Gtxn[1].close_remainder_to() == Global.zero_address(),
+                    # second account has opted-in
+                    App.optedIn(Int(1), Int(0)),
+                    is_empty(Int(0)),
+                    is_empty(Int(1)),
+                    # commitment
+                    Txn.application_args.length() == Int(2),
+                )
+            ),
+            App.localPut(Txn.sender(), local_opponent, Txn.accounts[1]),
+            App.localPut(Txn.sender(), local_wager, Gtxn[1].amount()),
+            App.localPut(
+                Txn.sender(),
+                local_commitment,
+                Txn.application_args[1],
+            ),
+            Approve(),
+        )
+
 
 	@Subroutine(TealType.none)
 	def accept_challenge():
